0e67da7434c4fbd71005ce26d4d1984f71841bd8
lvaudor <lise.vaudor@ens-lyon.fr>
2021-12-15 10:51:39 +0100

    update due to reformatting of web page

--- R/weather_date_station.R
+++ R/weather_date_station.R
@@ -16,54 +16,57 @@ weather_date_station=function(date_ymd,station_name,station_id){
   variables=content %>% 
     rvest::html_nodes("thead") %>% 
     rvest::html_nodes("th") %>% 
-    rvest::html_text()
+    rvest::html_text() %>% 
+    .[3:length(.)]
   rows= content %>%
     rvest::html_nodes("tbody") %>% 
     rvest::html_children()%>%
-    purrr::map(html_children) %>%
-    purrr::map(html_text) %>% 
+    purrr::map(rvest::html_children) %>%
+    purrr::map(rvest::html_text) %>% 
     purrr::map(as.matrix) %>% 
     purrr::map(t) %>% 
-    purrr::map(as_tibble)  %>% 
-    bind_rows() %>% 
+    purrr::map(tibble::as_tibble,.name_repair="minimal")  %>% 
+    dplyr::bind_rows() %>% 
+    .[,3:ncol(.)] %>% 
     magrittr::set_colnames(variables)
   date_et_heure=content %>%
     rvest::html_nodes("tbody") %>% 
     rvest::html_children()%>%
-    purrr::map(html_children) %>% 
+    purrr::map(rvest::html_children) %>% 
     purrr::map(purrr::pluck,1) %>% 
-    purrr::map(html_nodes,".tipsy-trigger") %>% 
-    purrr::map(html_attr,"title")
+    purrr::map(rvest::html_nodes,".tipsy-trigger") %>% 
+    purrr::map(rvest::html_attr,"title")
   date=date_et_heure%>% 
-    purrr::map(str_extract,"(?<=>).*(?=<br)") %>% 
+    purrr::map(stringr::str_extract,"(?<=>).*(?=<br)") %>% 
     purrr::map_chr(~.[!is.na(.)])
   heure=date_et_heure %>% 
-    purrr::map(str_extract,"(?<=<b>).*(?=</b)") %>% 
+    purrr::map(stringr::str_extract,"(?<=<b>).*(?=</b)") %>% 
     purrr::map(~.[!is.na(.)]) %>% 
-    purrr::map_chr(str_extract,".*(?=\\sUTC)")
-  timestamp=purrr::map2(date,heure,str_c,sep=" ")
-  tib_weather=tibble(timestamp=timestamp,
-                     date=date,
-                     time=heure) %>% 
-    bind_cols(rows) %>% 
-    mutate(timestamp=lubridate::dmy_hm(timestamp))
+    purrr::map_chr(stringr::str_extract,".*(?=\\sUTC)")
+  timestamp=purrr::map2(date,heure,stringr::str_c,sep=" ")
+  tib_weather=tibble::tibble(timestamp=timestamp,
+                             date=date,
+                             time=heure) %>% 
+    dplyr::bind_cols(rows) %>% 
+    dplyr::mutate(timestamp=lubridate::dmy_hm(timestamp))
   tib_weather=tib_weather %>% 
     dplyr::transmute(
                   timestamp=timestamp,
                   temperature=.$Température,
                   rain=.$Pluie,
                   wetness=.$Humidité,
-                  dew_point=.$`Pt. de rosée`,
-                  wind=.$`Vent moyen (raf.)`,
+                  dew_point=.[[10]],
+                  wind=.$`Vent`,
                   pressure=.$Pression) %>% 
     dplyr::mutate(temperature=stringr::str_replace(temperature," °C",""),
                   rain=stringr::str_replace(rain,"(?<=\\s).*",""),
                   wetness=stringr::str_replace(wetness,"%",""),
                   dew_point=stringr::str_replace(dew_point," °C",""),
-                  wind_gusts=stringr::str_extract(wind,"(?<=(h\\())[\\d\\.]*"),
+                  wind_gusts=stringr::str_extract(wind,"(?<=hraf\\.)[\\d\\.]*"),
                   wind_average=stringr::str_extract(wind,"\\d*(?=(\\skm))"),
                   pressure=stringr::str_replace(pressure,"hPa","")) %>% 
-    dplyr::mutate(rain=stringr::str_replace(rain,"\\s",""),
+    dplyr::mutate(temperature=stringr::str_extract(temperature,"^[-\\.0-9]"),
+                  rain=stringr::str_replace(rain,"\\s",""),
                   pressure=stringr::str_replace(pressure,"=","")) %>% 
     dplyr::select(-wind) %>%
     dplyr::mutate_at(.funs="as.numeric",.vars=dplyr::vars(-timestamp))
